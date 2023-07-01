1f11c5b3dd99110330f353b52964ec32883fed32
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-07-01 22:04:51 +0300

    change Main class

--- src/main/java/LessonOne/Main.java
+++ src/main/java/LessonOne/Main.java
@@ -1,10 +1,8 @@
 package LessonOne;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Object> pet = new ArrayList<>();
+
         Animal animalOne = new Cat("Лиза", "Scotland", 1, "grey", 5);
         Animal animalTwo = new Cat("Маня", "Scotland", 9, "grey", 5);
         Animal animalThird = new Dog("Рик", "Rottweiler", 2, "black", 10);
@@ -12,21 +10,19 @@ public class Main {
         Animal animalFifth = new Rat("Шапокляк", "Ноунэйм", 1, "white", 1);
         Animal animalSix = new Hamster("Ноунэйм", "Ноунэйм", 1, "yellow", 1);
 
-//        pet.add(animalOne);
-//        pet.add(animalTwo);
-//        pet.add(animalThird);
-//        pet.add(animalFour);
-//        pet.add(animalFifth);
-//        pet.add(animalSix);
-//
-//        System.out.println(pet);
-
-
-
-
         System.out.println(animalOne);
+        System.out.println(animalTwo);
+        System.out.println(animalThird);
+        System.out.println(animalFour);
+        System.out.println(animalFifth);
+        System.out.println(animalSix);
         animalOne.play();
         animalOne.say();
+        animalTwo.say();
+        animalThird.say();
+        animalFour.say();
+        animalFifth.say();
+        animalSix.say();
         animalOne.nutrition(6, animalOne.getRequiredAmountFood());
 
     }
