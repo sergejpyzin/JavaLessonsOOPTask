5ed7fca472bdae16e4b2c7f3d495a79c628ba1f0
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-07-11 13:36:04 +0300

    change class Box

--- src/main/java/LessonFour/Box.java
+++ src/main/java/LessonFour/Box.java
@@ -1,4 +1,28 @@
 package LessonFour;
 
-public class Box {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private final List<T> boxes;
+
+    public Box(List<T> boxes) {
+        this.boxes = boxes;
+    }
+
+    public Box(){
+        boxes = new ArrayList<>();
+    }
+
+    public void addFruit(T t){
+        boxes.add(t);
+    }
+    public int getBoxWeight(){
+        int boxWeight = 0;
+        for (T t:
+             boxes) {
+            boxWeight += t.getWeight();
+        }
+        return boxWeight;
+    }
 }
---
+++ src/main/java/LessonFour/Main.java
@@ -0,0 +1,7 @@
+package LessonFour;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
