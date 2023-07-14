9159713f4ad2d92e5198dc4d25f2d7310642db57
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-07-14 12:37:21 +0300

    created TeacherRepository and changed UserRepository

--- src/main/java/LessonsFive/repositories/UserRepository.java
+++ src/main/java/LessonsFive/repositories/UserRepository.java
@@ -12,5 +12,6 @@ public interface UserRepository <T extends User> {
 
     int remove(String fullName);
 
-    List<Student> getAllByGroupTitle(String groupTitle);
+    List<T> getAllByGroupTitle(String groupTitle);
+
 }
---
+++ src/main/java/LessonsFive/services/TeacherService.java
@@ -0,0 +1,4 @@
+package LessonsFive.services;
+
+public class TeacherService {
+}
