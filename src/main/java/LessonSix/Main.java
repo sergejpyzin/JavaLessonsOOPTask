56236c5dca29e82c04809d7186eed22e8ce7d15b
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-07-19 15:05:08 +0300

    Rename Main in TestMain and added Main in LessonSix

---
+++ src/main/java/LessonSix/Main.java
@@ -0,0 +1,7 @@
+package LessonSix;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/LessonSix/Work3/TestMain.java
+++ src/main/java/LessonSix/Work3/TestMain.java
@@ -1,6 +1,6 @@
 package LessonSix.Work3;
 
-public class Main {
+public class TestMain {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.getGreet("formal"));
